196227598f39a47c0c9670ae61b558c770595334
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-15 12:22:16 +0100

    inital commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿List<string> languageList = new List<string>();
+languageList.Add("HTML");
+languageList.Add("CSS");
+languageList.Add("Javacrit");
+languageList.Add("jQuery");
+languageList.Add("PHP");
+languageList.Add("Bootstrap");
+languageList.Add("Java");
+foreach (var item in languageList)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine();
+Console.WriteLine(languageList[3]);
+Console.WriteLine();
+Console.WriteLine(languageList[4]);
+Console.WriteLine();
+languageList.Remove("Bootstrap");
+languageList[2] = "Javascript";
+languageList.Add("C");
+foreach (var item in languageList)
+{
+    Console.WriteLine(item);
+}
