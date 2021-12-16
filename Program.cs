d4b4d5aff4a090d7b0ef5e3392293913ee071c6b
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-16 09:21:32 +0100

    Rendu le programme plus lisible pour l'utilisateur

--- Program.cs
+++ Program.cs
@@ -14,15 +14,16 @@ foreach (var item in languageList)
 }
 // Affichage d'éléments de la liste
 Console.WriteLine();
-Console.WriteLine(languageList[3]);
+Console.WriteLine($"Voici le 4ème élément de la liste : {languageList[3]}.");
 Console.WriteLine();
-Console.WriteLine(languageList[4]);
+Console.WriteLine($"Voici l’élément à l’index 4 de la liste : {languageList[4]}.");
 Console.WriteLine();
 // Modification de la liste
 languageList.Remove("Bootstrap");
 languageList[languageList.IndexOf("Javacrit")] = "Javascript";
 languageList.Add("C");
 // Affichage de la liste
+Console.WriteLine($"Affichage de toute la liste. La liste a été modifiée.");
 foreach (var item in languageList)
 {
     Console.WriteLine(item);
