b2975181ce25b9a8ec8d55342b1420fda79b6c7e
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-16 09:11:19 +0100

    Ajout de commentaires pour le programmeur

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿List<string> languageList = new List<string>();
+// Ajouté manuel d'éléments dans la liste
 languageList.Add("HTML");
 languageList.Add("CSS");
 languageList.Add("Javacrit");
@@ -6,18 +7,22 @@ languageList.Add("jQuery");
 languageList.Add("PHP");
 languageList.Add("Bootstrap");
 languageList.Add("Java");
+// Affichage de la liste
 foreach (var item in languageList)
 {
     Console.WriteLine(item);
 }
+// Affichage d'éléments de la liste
 Console.WriteLine();
 Console.WriteLine(languageList[3]);
 Console.WriteLine();
 Console.WriteLine(languageList[4]);
 Console.WriteLine();
+// Modification de la liste
 languageList.Remove("Bootstrap");
 languageList[languageList.IndexOf("Javacrit")] = "Javascript";
 languageList.Add("C");
+// Affichage de la liste
 foreach (var item in languageList)
 {
     Console.WriteLine(item);
